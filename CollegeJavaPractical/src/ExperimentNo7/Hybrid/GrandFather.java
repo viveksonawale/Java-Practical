6e36cce59da0f2a5fb3c9fd1f8ac1ab251e300c6
Vivek Sonawale <sonawalevivek777@gmail.com>
2024-09-12 21:10:57 +0530

    Added the Experiment No 8

--- CollegeJavaPractical/src/ExperimentNo7/Hybrid/GrandFather.java
+++ CollegeJavaPractical/src/ExperimentNo7/Hybrid/GrandFather.java
@@ -1,19 +1,34 @@
 package ExperimentNo7.Hybrid;
 
 public class GrandFather {
-
+    String variableGrandFather = "this is the grandfather class";
 }
 
 class Father extends GrandFather {
+    String variableFather = "this is the Father class";
 }
 
-class Child extends Father {
-
+class Child1 extends Father {
+    String vairbleChild1 = "this is first child class";
+}
+class Child2 extends Father{
+    String variableChild2 = "this is second child class";
 }
 
 class Main {
     public static void main(String[] args) {
+        Father father = new Father();
+        Child1 child1 = new Child1();
+        Child2 child2 = new Child2();
+        System.out.println(father.variableGrandFather);
 
+        System.out.println(child1.variableFather);
+
+//        printing the variable from the father class
+        System.out.println(child1.variableFather);
+        System.out.println(child1.vairbleChild1);
+        System.out.println(child2.variableFather);
+        System.out.println(child2.variableChild2);
     }
 }
 
---
+++ CollegeJavaPractical/src/ExperimentNo8/MultipleInheritance.java
@@ -0,0 +1,35 @@
+package ExperimentNo8;
+
+interface A {
+    public void thisIsAFunction();
+}
+
+interface B {
+    public void thisIsBFunction();
+}
+
+class C implements A, B {
+
+    @Override
+    public void thisIsAFunction() {
+        System.out.println("This is the Interface A");
+    }
+
+    @Override
+    public void thisIsBFunction() {
+        System.out.println("This is the Interface B");
+    }
+
+    public void thisIsCFunction(){
+        System.out.println("This is the Class C");
+    }
+}
+
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        C c = new C();
+        c.thisIsAFunction();
+        c.thisIsBFunction();
+        c.thisIsCFunction();
+    }
+}
